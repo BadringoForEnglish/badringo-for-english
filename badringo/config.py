a04f367c2b1a6360856f4024017df536553d03f0
badre <fouziibadr49@gmail.com>
2026-08-15 16:50:32 +0100

    Version 1.2.2

--- badringo/config.py
+++ badringo/config.py
@@ -7,7 +7,7 @@ import os
 import json
 
 APP_NAME = "Badringo for English"
-APP_VERSION = "1.2.1"
+APP_VERSION = "1.2.2"
 
 # Dossiers
 # En version .exe (PyInstaller --onefile), le programme s'exécute depuis un
